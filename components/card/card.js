24db1654ffd9fe85b4a9cb7f5b4b2c7339191841
Veronika Popova <freebird130188@gmail.com>
2023-03-20 10:27:49 +0100

    Added card.js

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,26 @@
-export function createCharacterCard() {}
+export function createCharacterCard(imageUrl, name, status, type, occurrences) {
+  const cards = document.querySelector('[data-js="card-container"]');
+
+  cards.innerHTML += `
+  <li class="card">
+  <div class="card__image-container">
+    <img
+      class="card__image"
+      src="${imageUrl}"
+      alt="Rick Sanchez"
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${name}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${status}</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${type}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${occurrences}</dd>
+    </dl>
+  </div>
+  </li>`;
+}
