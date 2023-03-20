a8e0aa6b67e1e42ee5493fb6d0d4e64632960f3f
Xaver Schüll <botacode@gmail.com>
2023-03-20 12:11:39 +0100

    Just to check out until we resolve this

--- index.js
+++ index.js
@@ -14,3 +14,38 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const cardsUrl =
+  "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20";
+
+const fetchCharacters = async () => {
+  try {
+    const response = await fetch(cardsUrl);
+    const jsonData = await response.json();
+    return jsonData;
+  } catch {
+    console.log("Something went wrong");
+  }
+};
+fetchCharacters();
+
+console.log("fetchCharacters", fetchCharacters());
+console.log("fetchCharacters", typeof fetchCharacters());
+
+// jsonData.forEach((character) => {
+//   const image = character.image;
+//   console.log(character.image);
+//   const name = character.name;
+//   console.log(character.name);
+//   const status = character.status;
+//   console.log(character.status);
+//   const type = character.type;
+//   console.log(character.type);
+//   const occurrences = character.occurrences;
+//   console.log(character.occurrences);
+
+//   const characterData = [];
+//   characterData.push(image, name, status, type, occurrences);
+
+//   characters.push(characterData);
+// });
